23c4eed36708c9c417ff1b283dfdedf791a34c09
Alicia Lam <66523331+loser564@users.noreply.github.com>
2023-10-02 21:58:41 +0800

    Add files via upload

---
+++ SC2002/Java/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class main {
+   public static void main(String[] args){
+      String stringA = " Wild ";
+
+      String stringB = " Irish ";
+
+      String stringC = " Rose ";
+
+      String result = stringA.trim() + stringB + stringC.trim();
+
+   }      
+}
